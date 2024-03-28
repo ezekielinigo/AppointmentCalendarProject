8413da81be455d6b046d8434027defc83af4b1fd
unknown <sean_macariola@dlsu.edu.ph>
2024-03-28 20:20:52 +0800

    requirements.txt for railway?

--- AppointmentCalendarProject/settings.py
+++ AppointmentCalendarProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r6v%t7be!3o$vq430qh_#he#d7tl!ekbwsf83dr_8*j_#%udcm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,7 +117,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+# STATIC FILES = ASSETS
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn AppointmentCalendarProject.wsgi --log-file -
